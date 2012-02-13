a5193519c76251a77d5d8d9ed4958634e1d44bf0
Christopher Benninger <chris@claudus>
2012-02-13 10:44:29 -0800

    Just fiddling

--- flo.py
+++ flo.py
@@ -4,34 +4,40 @@ hotkey_str = "Alt-r"
 default_operand = 0
 
 def go():
-    sEA = ScreenEA()
-    operand = GetOpnd(sEA,default_operand)
-    operand_val = GetOperandValue(sEA,default_operand)
-    print "Operand: "+operand
-    if operand_val:
-        print "Value: "+operand_val
-    #print operand_str+": "+str(operand_val)
-    #print GetDisasm(ScreenEA())
+        print "GO"
+        sEA = ScreenEA()
+        operand = GetOpnd(sEA,default_operand)
+        operand_val = GetOperandValue(sEA,default_operand)
+        print "Operand: "+str(operand)
+        if operand_val:
+            print "Value: "+str(operand_val)
 
+def go_callback(*args):
+        go()
+        return 1
 
+# IDA binds hotkeys to IDC functions so a trampoline IDC function must be created
+idaapi.CompileLine('static flopy_key() { RunPythonStatement("go()"); }')
+add_idc_hotkey(hotkey_str, 'flopy_key')
+
+# Add menu item
 try:
-    del_idc_hotkey(hotkey_str)
+    if ctx:
+        idaapi.del_menu_item(ctx)
 except:
     pass
 
-# IDA binds hotkeys to IDC functions so a trampoline IDC function must be created
-idaapi.CompileLine('static sobpy_key() { RunPythonStatement("go()"); }')
-
-# Add the hotkey
-if add_idc_hotkey(hotkey_str, 'sobpy_key'):
-      print "Registered "+hotkey_str+" as hotkey"
-
-
+ctx = idaapi.add_menu_item("Search/", "Go", "", 0, go_callback, tuple("hello world"))
+if ctx is None:
+    print "Failed to add menu!"
+    del ctx
+else:
+    print "Menu added successfully!"
 
 
-   #  ua_ana0(get_screen_ea());
-   #AnalyseArea((), eEA)
-   #  if (get_nb_op())
+#  ua_ana0(get_screen_ea());
+#AnalyseArea((), eEA)
+#  if (get_nb_op())
 #    num = getOpNum();
 #    if ( !(cmd.Operands[0].showed()) )
 #    op = new df_op_t(num);
